720206f4be3186db03a1ca8b0122b318b9646d9a
B4zo <aljaz-skafar@outlook.com>
2025-10-01 19:13:44 +0200

    Vaja 4 do Naloga 13

--- Vaja04-AljazSkafar/Vaja04-AljazSkafar/Form1.Designer.cs
+++ Vaja04-AljazSkafar/Vaja04-AljazSkafar/Form1.Designer.cs
@@ -29,99 +29,141 @@
         private void InitializeComponent()
         {
             this.grpVstavljanjeStevil = new System.Windows.Forms.GroupBox();
-            this.grpFunkcije = new System.Windows.Forms.GroupBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btnVstaviStevilo = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.grpFunkcije = new System.Windows.Forms.GroupBox();
+            this.btnVsota = new System.Windows.Forms.Button();
+            this.btnPovprecje = new System.Windows.Forms.Button();
             this.listBox = new System.Windows.Forms.ListBox();
+            this.btnPocisti = new System.Windows.Forms.Button();
+            this.btnMinMax = new System.Windows.Forms.Button();
             this.grpVstavljanjeStevil.SuspendLayout();
             this.grpFunkcije.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpVstavljanjeStevil
             // 
-            this.grpVstavljanjeStevil.Controls.Add(this.textBox1);
-            this.grpVstavljanjeStevil.Controls.Add(this.button1);
+            this.grpVstavljanjeStevil.Controls.Add(this.textBox);
+            this.grpVstavljanjeStevil.Controls.Add(this.btnVstaviStevilo);
             this.grpVstavljanjeStevil.Controls.Add(this.label1);
-            this.grpVstavljanjeStevil.Location = new System.Drawing.Point(12, 12);
+            this.grpVstavljanjeStevil.Location = new System.Drawing.Point(18, 18);
+            this.grpVstavljanjeStevil.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.grpVstavljanjeStevil.Name = "grpVstavljanjeStevil";
-            this.grpVstavljanjeStevil.Size = new System.Drawing.Size(140, 159);
+            this.grpVstavljanjeStevil.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grpVstavljanjeStevil.Size = new System.Drawing.Size(210, 245);
             this.grpVstavljanjeStevil.TabIndex = 0;
             this.grpVstavljanjeStevil.TabStop = false;
             this.grpVstavljanjeStevil.Text = "Vstavljanje števil";
             // 
-            // grpFunkcije
+            // textBox
             // 
-            this.grpFunkcije.Controls.Add(this.button3);
-            this.grpFunkcije.Controls.Add(this.button2);
-            this.grpFunkcije.Location = new System.Drawing.Point(12, 190);
-            this.grpFunkcije.Name = "grpFunkcije";
-            this.grpFunkcije.Size = new System.Drawing.Size(140, 159);
-            this.grpFunkcije.TabIndex = 1;
-            this.grpFunkcije.TabStop = false;
-            this.grpFunkcije.Text = "Funkcije";
+            this.textBox.Location = new System.Drawing.Point(93, 83);
+            this.textBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(103, 26);
+            this.textBox.TabIndex = 3;
+            // 
+            // btnVstaviStevilo
+            // 
+            this.btnVstaviStevilo.Location = new System.Drawing.Point(26, 146);
+            this.btnVstaviStevilo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnVstaviStevilo.Name = "btnVstaviStevilo";
+            this.btnVstaviStevilo.Size = new System.Drawing.Size(150, 54);
+            this.btnVstaviStevilo.TabIndex = 2;
+            this.btnVstaviStevilo.Text = "Vstavi število";
+            this.btnVstaviStevilo.UseVisualStyleBackColor = true;
+            this.btnVstaviStevilo.Click += new System.EventHandler(this.btnVstaviStevilo_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 57);
+            this.label1.Location = new System.Drawing.Point(21, 88);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.Size = new System.Drawing.Size(60, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Število:";
             // 
-            // button2
+            // grpFunkcije
             // 
-            this.button2.Location = new System.Drawing.Point(17, 38);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 35);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.grpFunkcije.Controls.Add(this.btnMinMax);
+            this.grpFunkcije.Controls.Add(this.btnVsota);
+            this.grpFunkcije.Controls.Add(this.btnPovprecje);
+            this.grpFunkcije.Location = new System.Drawing.Point(18, 292);
+            this.grpFunkcije.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grpFunkcije.Name = "grpFunkcije";
+            this.grpFunkcije.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grpFunkcije.Size = new System.Drawing.Size(210, 245);
+            this.grpFunkcije.TabIndex = 1;
+            this.grpFunkcije.TabStop = false;
+            this.grpFunkcije.Text = "Funkcije";
             // 
-            // button3
+            // btnVsota
             // 
-            this.button3.Location = new System.Drawing.Point(17, 92);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 35);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnVsota.Location = new System.Drawing.Point(35, 42);
+            this.btnVsota.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnVsota.Name = "btnVsota";
+            this.btnVsota.Size = new System.Drawing.Size(150, 54);
+            this.btnVsota.TabIndex = 1;
+            this.btnVsota.Text = "Vsota";
+            this.btnVsota.UseVisualStyleBackColor = true;
+            this.btnVsota.Click += new System.EventHandler(this.btnVsota_Click);
             // 
-            // button1
+            // btnPovprecje
             // 
-            this.button1.Location = new System.Drawing.Point(17, 95);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 35);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(62, 54);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(70, 20);
-            this.textBox1.TabIndex = 3;
+            this.btnPovprecje.Location = new System.Drawing.Point(35, 106);
+            this.btnPovprecje.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnPovprecje.Name = "btnPovprecje";
+            this.btnPovprecje.Size = new System.Drawing.Size(150, 54);
+            this.btnPovprecje.TabIndex = 0;
+            this.btnPovprecje.Text = "Povprečje";
+            this.btnPovprecje.UseVisualStyleBackColor = true;
+            this.btnPovprecje.Click += new System.EventHandler(this.btnPovprecje_Click);
             // 
             // listBox
             // 
             this.listBox.FormattingEnabled = true;
-            this.listBox.Location = new System.Drawing.Point(158, 12);
+            this.listBox.ItemHeight = 20;
+            this.listBox.Location = new System.Drawing.Point(237, 18);
+            this.listBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(120, 342);
+            this.listBox.Size = new System.Drawing.Size(178, 444);
             this.listBox.TabIndex = 2;
+            // 
+            // btnPocisti
+            // 
+            this.btnPocisti.Enabled = false;
+            this.btnPocisti.Location = new System.Drawing.Point(250, 483);
+            this.btnPocisti.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnPocisti.Name = "btnPocisti";
+            this.btnPocisti.Size = new System.Drawing.Size(150, 54);
+            this.btnPocisti.TabIndex = 2;
+            this.btnPocisti.Text = "Pocisti";
+            this.btnPocisti.UseVisualStyleBackColor = true;
+            this.btnPocisti.Click += new System.EventHandler(this.btnPocisti_Click);
+            // 
+            // btnMinMax
+            // 
+            this.btnMinMax.Location = new System.Drawing.Point(35, 170);
+            this.btnMinMax.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnMinMax.Name = "btnMinMax";
+            this.btnMinMax.Size = new System.Drawing.Size(150, 54);
+            this.btnMinMax.TabIndex = 2;
+            this.btnMinMax.Text = "Min/Max";
+            this.btnMinMax.UseVisualStyleBackColor = true;
+            this.btnMinMax.Click += new System.EventHandler(this.btnMinMax_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.ClientSize = new System.Drawing.Size(426, 555);
+            this.Controls.Add(this.btnPocisti);
             this.Controls.Add(this.listBox);
             this.Controls.Add(this.grpFunkcije);
             this.Controls.Add(this.grpVstavljanjeStevil);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Učenje imam rad";
@@ -136,12 +178,14 @@
 
         private System.Windows.Forms.GroupBox grpVstavljanjeStevil;
         private System.Windows.Forms.GroupBox grpFunkcije;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnVstaviStevilo;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnVsota;
+        private System.Windows.Forms.Button btnPovprecje;
+        private System.Windows.Forms.TextBox textBox;
         private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnPocisti;
+        private System.Windows.Forms.Button btnMinMax;
     }
 }
 
--- Vaja04-AljazSkafar/Vaja04-AljazSkafar/Form1.cs
+++ Vaja04-AljazSkafar/Vaja04-AljazSkafar/Form1.cs
@@ -15,10 +15,114 @@ namespace Vaja04_AljazSkafar
         public const int listBoxMax = 15;
         public const int minUserInput = 1;
         public const int maxUserInput = 20;
+        private bool jeSteviloValidno(string stevilo, int min, int max) 
+        {
+            if(int.TryParse(stevilo, out int value) && (value >= min && value <= max))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        private int sestej(System.Windows.Forms.ListBox.ObjectCollection seznam)
+        {
+            int vsota = 0;
+            foreach (string item in seznam)
+            {
+                vsota += int.Parse(item);
+            }
+            return vsota;
+        }
+        private double povprecje(System.Windows.Forms.ListBox.ObjectCollection seznam)
+        {
+            return (double)sestej(seznam) / seznam.Count;
+        }
         public Form1()
         {
             InitializeComponent();
         }
+        private void btnVstaviStevilo_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text.Length == 0)
+            {
+                MessageBox.Show("Nisi vnesel števila.\n\nZa začetek najprej vnesi kakšno število.", "Napaka", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (jeSteviloValidno(textBox.Text, minUserInput, maxUserInput))
+                {
+                    if (listBox.Items.Count < listBoxMax)
+                    {
+                        listBox.Items.Add(textBox.Text);
+                        btnPocisti.Enabled = true;
+                        textBox.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Dosegel si maksimalno število vnosov.\n\nMaksimalno število vnosov je " + listBoxMax + ".", "Napaka", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Število ni veljavno.\n\nVnesi število med " + minUserInput + " in " + maxUserInput + ".", "Napaka", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox.SelectAll();
+                }
+            }
+        }
+        private void btnVsota_Click(object sender, EventArgs e)
+        {
+            if (listBox.Items.Count == 0)
+            {
+                MessageBox.Show("Vsote ne moreš izračunati, saj nisi vnesel števil.\n\nZa Začetek najprej vnesi kakšno število.", "Napaka", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("V listBox-u je: " + listBox.Items.Count + " števil.\n\n" + "Vsota vseh vnesenih števil je: " + sestej(listBox.Items), "Vsota (int)", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                listBox.Items.Clear();
+                btnPocisti.Enabled = false;
+            }
+        }
+        private void btnPovprecje_Click(object sender, EventArgs e)
+        {
+            if (listBox.Items.Count == 0)
+            {
+                MessageBox.Show("Nisi vnesel števila.\n\nZa začetek najprej vnesi kakšno število.", "Napaka", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("V listBox-u je: " + listBox.Items.Count + " števil.\n\n" + "Povprecje vseh vnesenih števil je: " + povprecje(listBox.Items), "Povprečje (double)", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                listBox.Items.Clear();
+                btnPocisti.Enabled = false;
+            }
+        }
 
+        private void btnPocisti_Click(object sender, EventArgs e)
+        {
+            textBox.Clear();
+            listBox.Items.Clear();
+            btnPocisti.Enabled = false;
+        }
+
+        private void btnMinMax_Click(object sender, EventArgs e)
+        {
+            int max = 0;
+            foreach (string item in listBox.Items)
+            {
+                if (int.Parse(item) > max)
+                    max = int.Parse(item);
+                else
+                    continue;
+            }
+            int min = 20;
+            foreach (string item in listBox.Items)
+            {
+                if (int.Parse(item) < min)
+                    min = int.Parse(item);
+                else
+                    continue;
+            }
+            MessageBox.Show("Minimum je: " + min + "\n\n" + "Maximum je: " + max + "\n\n" , "Min/max", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
